379d0ec9ea7803ccf7610753d30fd7bff05e5c89
p6 <puppypepper21@gmail.com>
2024-11-02 02:17:54 +0900

    Add line_num

--- src/list2-18.rs
+++ src/list2-18.rs
@@ -5,9 +5,14 @@ Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
+
+    let mut line_num: usize = 1;
+
     for line in quote.lines() {
         if line.contains(search_item) {
-            println!("{}", line);
+            println!("{}: {}", line_num, line);
         }
+
+        line_num += 1;
     }
 }
