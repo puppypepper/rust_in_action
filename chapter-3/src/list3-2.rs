9eb53eb93e0dcd611fbe568aa79dd26d2361fc90
p6 <puppypepper21@gmail.com>
2024-11-15 02:11:29 +0900

    Start chapter3

---
+++ chapter-3/src/list3-2.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+fn main() {
+    let mut f1 = File {
+        name: String::from("f1.txt"),
+        data: Vec::new(),
+    };
+
+    let f1_name = &f1.name;
+    let f1_length = &f1.data.len();
+
+    println!("{:?}", f1);
+    println!("{:?}", f1_name);
+    println!("{:?}", f1_length);
+
+    f1.data = vec![114, 117, 115, 116];
+
+    println!("{:?}", f1);
+    println!("{:?}", f1_name);
+    println!("{:?}", f1_length);
+}
