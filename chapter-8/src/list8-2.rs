9fb4aac8d6e05805660be10fb562fb109474ecaa
papi <puppypepper21@gmail.com>
2025-05-01 01:21:20 +0900

    Add list 8-2.rs

---
+++ chapter-8/src/list8-2.rs
@@ -0,0 +1,9 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "http://www.rustinaction.com/";
+    let mut response = reqwest::get(url)?;
+
+    let content = response.text()?;
+    print!("{}", content);
+
+    Ok(())
+}
